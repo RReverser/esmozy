9fea91b1a44778d6f1d86e88985dc62ee9d5fd22
Ingvar Stepanyan <me@rreverser.com>
2017-06-02 18:08:13 +0100

    Store Reflect.parse and builder globally

--- src/script.js
+++ src/script.js
@@ -1,33 +1,39 @@
-(function parse(source) {
+'use strict';
+
+const { parse } = Reflect;
+
+const builder = {
+    literal(value, loc) {
+        if (typeof value === 'object' && value !== null) {
+            // regexp only
+            var flags = '';
+            if (value.global) flags += 'g';
+            if (value.ignoreCase) flags += 'i';
+            if (value.multiline) flags += 'm';
+            if (value.sticky) flags += 'y';
+            return {
+                loc,
+                type: 'Literal',
+                value: null,
+                regex: {
+                    pattern: value.source,
+                    flags
+                }
+            };
+        } else {
+            return {
+                loc,
+                type: 'Literal',
+                value
+            };
+        }
+    }
+};
+
+(source => {
     try {
         return Reflect.parse(source, {
-            builder: {
-                literal(value, loc) {
-                    if (typeof value === 'object' && value !== null) {
-                        // regexp only
-                        var flags = '';
-                        if (value.global) flags += 'g';
-                        if (value.ignoreCase) flags += 'i';
-                        if (value.multiline) flags += 'm';
-                        if (value.sticky) flags += 'y';
-                        return {
-                            loc,
-                            type: 'Literal',
-                            value: null,
-                            regex: {
-                                pattern: value.source,
-                                flags
-                            }
-                        };
-                    } else {
-                        return {
-                            loc,
-                            type: 'Literal',
-                            value
-                        };
-                    }
-                }
-            }
+            builder
         });
     } catch ({ message, fileName, lineNumber }) {
         return {
